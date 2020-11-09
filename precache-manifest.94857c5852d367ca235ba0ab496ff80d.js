edc0caddaa83bee106e37e77cab8f58805ab869e
caron1211 <caron1211@gmail.com>
2020-11-09 11:27:09 +0200

    Updates

---
+++ precache-manifest.94857c5852d367ca235ba0ab496ff80d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b2a38a49de03ad61c37358aaa19177",
+    "url": "/smart-flower/index.html"
+  },
+  {
+    "revision": "010f33ec64ba39e38291",
+    "url": "/smart-flower/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "74b4f291f900e9bece0f",
+    "url": "/smart-flower/static/css/main.54277825.chunk.css"
+  },
+  {
+    "revision": "010f33ec64ba39e38291",
+    "url": "/smart-flower/static/js/2.29a6cabc.chunk.js"
+  },
+  {
+    "revision": "99c1bf48081ed736382f4cc79d8323d8",
+    "url": "/smart-flower/static/js/2.29a6cabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b4f291f900e9bece0f",
+    "url": "/smart-flower/static/js/main.22ab34fd.chunk.js"
+  },
+  {
+    "revision": "fff122d2fed96029982d",
+    "url": "/smart-flower/static/js/runtime-main.583d4b42.js"
+  },
+  {
+    "revision": "4100098c8af8b04589771aa58935f4c8",
+    "url": "/smart-flower/static/media/brain.4100098c.png"
+  }
+]);
